0e113c65abc34f3a7e561663e2a8f8558226ba61
Glitch (fcc3-header-parser) <none>
2017-10-23 22:50:38 +0000

    ✍️😀 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Request Header Parser Microservice
+___
+
+## User story:
+
+*  I can get the IP address, language and operating system for my browser.
+
+___
+
+You can see live version [here](https://fcc3-header-parser.glitch.me/).
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require('express');
+var getos = require('getos');
 var app = express();
 
 // we've started you off with Express, 
@@ -11,29 +12,31 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+var os;
+  
+getos(
+  (e, o) => {
+    if(e) console.log(e);
+    os = o.dist +" " +o.release;
+  }
+);
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/api/whoami", function (request, response) {
+  //console.log('api working');
+  
+  response.json({
+    ipaddress: request.ip,
+    Language: request.acceptsLanguages()[0],
+    os: os
+  });
+  
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
